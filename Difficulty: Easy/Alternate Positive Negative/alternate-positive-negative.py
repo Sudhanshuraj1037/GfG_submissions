c364bb38c4402ccdf7440796c8cd985af301494a
Sudhanshu Raj <sudhanshu78158@gmail.com>
2026-09-09 23:27:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.py
+++ Difficulty: Easy/Alternate Positive Negative/alternate-positive-negative.py
@@ -1,32 +1,30 @@
 class Solution:
     def rearrange(self,arr):
         # code here
-        # 1. Positive aur Negative numbers ko alag lists me store karein
         pos = [x for x in arr if x >= 0]
         neg = [x for x in arr if x < 0]
-
-        i = 0  # pos list ka index tracker
-        j = 0  # neg list ka index tracker
-        k = 0  # Original arr ka index tracker
-
-        # 2. Jab tak dono lists me elements hain, alternate karke arr me daalein
+        
+        i = 0
+        j = 0
+        k = 0
+        
         while i < len(pos) and j < len(neg):
             arr[k] = pos[i]
             k += 1
             i += 1
-
+            
             arr[k] = neg[j]
             k += 1
             j += 1
-
-        # 3. Agar positive numbers bach gaye hain, toh unhe bache hue arr me copy karein
+            
         while i < len(pos):
             arr[k] = pos[i]
             k += 1
             i += 1
-
-        # 4. Agar negative numbers bach gaye hain, toh unhe bache hue arr me copy karein
+            
         while j < len(neg):
             arr[k] = neg[j]
             k += 1
             j += 1
+            
+        
